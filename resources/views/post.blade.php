606586fbe40ccf6b509fa66227dc96bda4fbb916
vagrant <vagrant@homestead>
2019-10-23 12:40:33 +0000

    aula 7

---
+++ resources/views/post.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dados</title>
+</head>
+<body>
+    <h1>Posts</h1>
+    <p>{{ $post }}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,18 @@ Route::get('dados', function () {
     ]);
 });
 
+Route::get('post/{post}', function ($post) {
+   $posts = [
+        'my-first-post' => 'Hello, is the my first post',
+       'my-second-post' => 'Good, is the my second post'
+   ];
+
+   if (! array_key_exists($post, $posts)) {
+       abort(404, 'Desculpe, esta pagina não foi encontrada');
+   }
+
+   return view('post', [
+      'post' => $posts[$post]
+   ]);
+});
+
