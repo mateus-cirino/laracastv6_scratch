5080c07775736938888bf02c2cad0069f2d762ca
Mateus Cirino <vagrant@homestead>
2019-10-25 14:23:19 +0000

    aula 10

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function show ($slug) {
-        $post = DB::table('posts')->where('slug', $slug)->first();
-
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         return view('post', [
             'post' => $post
